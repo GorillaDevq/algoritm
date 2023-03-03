cdecb08e1e0f23a859db6716abfffbd756e64ce3
Ivan Mikhalev <gorilladev@MacBook-Air-Ivan.local>
2023-03-03 14:19:25 +0300

    binary & linear search

---
+++ linearSearch.js
@@ -0,0 +1,15 @@
+const arrray = [1,4,7,6,9,5,4,5,11,21]
+let count = 0
+function linearSearch(arr, item) {
+    for (let i = 0; i < arr.length; i++){
+        count += 1
+        if(arr[i] === item) {
+            return i;
+        }
+    }
+    return null
+}
+
+console.log(linearSearch(arrray, 67))
+console.log('count = ' + count)
+
---
+++ selectionSort.js
@@ -0,0 +1,9 @@
+const array = [0,1,4,213,63,7,4,-65,-4,2,3]
+let count = 0
+
+function selectionSort(arr) {
+    for (let index = 0; index < arr.length; index++) {
+        for(let i = 0; index < arr.length; index++){}
+        
+    }
+}
